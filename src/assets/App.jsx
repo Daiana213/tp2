8730c96a22fe56021d19b707a3efbfde11705db5
Daiana Palacios <daianapalacios213@gmail.com>
2025-04-16 18:58:49 -0300

    first commit

---
+++ src/assets/App.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import InputContrasena from "./componentes/InputContrasena";
+import BarraFortaleza from "./componentes/BarraFortaleza";
+import GeneradorContrasena from "./componentes/GeneradorContrasena";
+import PanelAvanzado from "./componentes/PanelAvanzado";
+import { obtener_fortaleza_contrasena, generar_contrasena } from "./utils";
+import './App.css';
+
+export default function App() {
+  const [contrasena, setContrasena] = useState("");
+  const [contrasenaGenerada, setContrasenaGenerada] = useState("");
+  const [mostrarContrasena, setMostrarContrasena] = useState(false);
+  const [mostrarPanelAvanzado, setMostrarPanelAvanzado] = useState(false);
+  const [longitud, setLongitud] = useState(12);
+  const [incluirMinusculas, setIncluirMinusculas] = useState(true);
+  const [incluirMayusculas, setIncluirMayusculas] = useState(true);
+  const [incluirNumeros, setIncluirNumeros] = useState(true);
+  const [incluirEspeciales, setIncluirEspeciales] = useState(false);
+  const fortaleza = obtener_fortaleza_contrasena(contrasena);
+
+  const copiarAlPortapapeles = () => {
+    navigator.clipboard.writeText(contrasenaGenerada)
+      .then(() => {
+        alert("¡Contraseña copiada al portapapeles!");
+      })
+      .catch(() => {
+        alert("Error al copiar la contraseña.");
+      });
+  };
+
+  const togglePanelAvanzado = () => {
+    setMostrarPanelAvanzado(!mostrarPanelAvanzado);
+  };
+
+  const generarNuevaContrasena = () => {
+    const nuevaContrasena = generar_contrasena(
+      longitud,
+      incluirMinusculas,
+      incluirMayusculas,
+      incluirNumeros,
+      incluirEspeciales
+    );
+    setContrasenaGenerada(nuevaContrasena);
+  };
+
+  return (
+    <div className="contenedor-principal">
+      <div className="contenedor-formulario">
+        <h2 className="titulo-formulario">Comprobador de Contraseña</h2>
+
+        <InputContrasena
+          contrasena={contrasena}
+          setContrasena={setContrasena}
+          mostrar={mostrarContrasena}
+          setMostrar={setMostrarContrasena}
+        />
+
+        <BarraFortaleza fortaleza={fortaleza} contrasena={contrasena} />
+
+        <button className="boton-panel-avanzado" onClick={togglePanelAvanzado}>
+          {mostrarPanelAvanzado ? 'Ocultar Opciones' : 'Mostrar Opciones Avanzadas'}
+        </button>
+
+        {mostrarPanelAvanzado && (
+          <PanelAvanzado
+            longitud={longitud}
+            setLongitud={setLongitud}
+            incluirMinusculas={incluirMinusculas}
+            setIncluirMinusculas={setIncluirMinusculas}
+            incluirMayusculas={incluirMayusculas}
+            setIncluirMayusculas={setIncluirMayusculas}
+            incluirNumeros={incluirNumeros}
+            setIncluirNumeros={setIncluirNumeros}
+            incluirEspeciales={incluirEspeciales}
+            setIncluirEspeciales={setIncluirEspeciales}
+          />
+        )}
+
+        <div className="generador-contrasena-container">
+          <button
+            onClick={generarNuevaContrasena}
+            className="generar-contrasena-boton"
+          >
+            Generar Contraseña
+          </button>
+          {contrasenaGenerada && (
+            <div className="contrasena-generada-div">
+              {contrasenaGenerada}
+              <button
+                className="boton-copiar"
+                onClick={copiarAlPortapapeles}
+              >
+                Copiar Contraseña
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
